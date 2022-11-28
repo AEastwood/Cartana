5997cc43f6400c7de3ff68acc2e0a3d9f7c6717c
Adam Eastwood <adam-3@live.co.uk>
2022-11-28 01:52:10 +0000

    Initial commit

---
+++ Cartana/src/hardware/hardware.cpp
@@ -0,0 +1,6 @@
+#include "hardware.h"
+#include "../core/log/log.h"
+
+Hardware::Hardware() {
+	Log("Hardware says hello :)");
+}
---
+++ Cartana/src/hardware/hardware.h
@@ -0,0 +1,11 @@
+
+#pragma once
+
+class Hardware {
+
+private:
+
+public:
+	Hardware();
+
+};
